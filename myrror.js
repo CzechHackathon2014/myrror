367c9541b7a9525f71395f8a1599826c122302b4
Libor Rysavy <libor.rysavy@gooddata.com>
2014-11-08 19:49:06 +0100

    add camera and routing

--- myrror.js
+++ myrror.js
@@ -1,34 +1,58 @@
 Questions = new Mongo.Collection("questions");
 
+Router.configure({
+    layoutTemplate: 'defaultLayout' // the default layout
+});
+
+Router.route('/', function () {
+    this.layout('defaultLayout');
+    this.render('upload');
+});
+Router.route('/rate');
+
 if (Meteor.isClient) {
 
-//    Template.body.helpers({
+//    Session.setDefault("counter", 0);
+
+//    Template.upload.helpers({
 //        tasks: function () {
 //            return Questions.find({});
+//        },
+//        counter: function () {
+//            return Session.get("counter");
 //        }
 //    });
 
-    Session.setDefault("counter", 0);
-
-    Template.upload.helpers({
-        counter: function () {
-            return Session.get("counter");
-        }
-    });
-
     Template.upload.events({
-        'click button .upload': function () {
-            Session.set("counter", Session.get("counter") + 1);
+        'click .upload': function () {
+//            Session.set("counter", Session.get("counter") + 1);
+        },
+        'click .takePicture' : function () {
+            var cameraOptions = {
+                width: 600,
+                height: 600
+            };
+
+            MeteorCamera.getPicture(cameraOptions, function (error, data) {
+                Session.set("photo", data);
+            });
         },
         'submit .question': function () {
             Questions.insert({
                 text: event.target.question.value,
                 createdAt: new Date()
+//                photo: Session.get("photo")
             });
 
             event.target.question.value = "";
-            // Prevent default form submit
-            return false;
+            Router.go("/rate");
+            return false; // Prevent default form submit
+        }
+    });
+
+    Template.rate.helpers({
+        photo: function () {
+            return Session.get("photo");
         }
     });
 
