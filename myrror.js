bf5d77880f8b7926fa2db471e7d96b803657b30e
Libor Rysavy <libor.rysavy@gooddata.com>
2014-11-09 13:07:28 +0100

    add results

--- myrror.js
+++ myrror.js
@@ -9,6 +9,7 @@ Router.route('/', function () {
     this.render('upload');
 });
 Router.route('/rate');
+Router.route('/result');
 
 if (Meteor.isClient) {
 
@@ -69,20 +70,87 @@ if (Meteor.isClient) {
 
     Template.rate.helpers({
         photo: function () {
-            return Questions.findOne({ "owner": {$ne: "$userId"}}, {skip: Session.get("counter")}).photo;
+            return Session.get("photo");
+        },
+        questionText: function () {
+            return Session.get("questionText");
         },
         counter: function () {
             return Session.get("counter");
         }
     });
 
-
     Template.rate.events({
-        'click .yes': function () {
+        'click #sayYes': function () {
+            Questions.update({"_id": Session.get("questionId")}, {'$addToSet': {"answerYes": Meteor.userId()}});
+            nextImage();
         },
-        'click .no': function () {
+        'click #sayNo': function () {
+            Questions.update({"_id": Session.get("questionId")}, {'$addToSet': {"answerNo": Meteor.userId()}});
+            nextImage();
         }
     });
+
+    Template.rate.created = function () {
+        nextImage();
+    };
+
+    function nextImage() {
+        var counter = Session.get("counter");
+        if (counter < 4) {
+            var question = Questions.findOne({ "owner": {$ne: "$userId"}}, {skip: counter});
+            Session.set("questionId", question._id);
+            Session.set("questionText", question.text);
+            Session.set("photo", question.photo);
+            Session.set("counter", counter + 1);
+        } else {
+            Router.go("/result");
+        }
+    }
+
+    Template.result.helpers({
+        photo: function () {
+            return Session.get("photo");
+        },
+        questionText: function () {
+            return Session.get("questionText");
+        },
+        yesCount: function () {
+            return Session.get("yesCount");
+        },
+        noCount: function () {
+            return Session.get("noCount");
+        },
+        counter: function () {
+            return Session.get("counter");
+        }
+    });
+
+    Template.result.created = function () {
+        Session.set("counter", 0);
+        nextResult();
+    };
+
+    function nextResult() {
+        var counter = Session.get("counter");
+        console.log("counter: " + counter + " q: " + question);
+        var question = Questions.findOne({}, {skip: counter});
+        console.log("counter: " + counter + " q: " + question);
+        Session.set("questionId", question._id);
+        Session.set("questionText", question.text);
+        if (question.answerYes) {
+            Session.set("yesCount", question.answerYes.length);
+        } else {
+            Session.set("yesCount", 0);
+        }
+        if (question.answerNo) {
+            Session.set("noCount", question.answerNo.length);
+        } else {
+            Session.set("noCount", 0);
+        }
+        Session.set("photo", question.photo);
+        Session.set("counter", counter + 1);
+    }
 
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
